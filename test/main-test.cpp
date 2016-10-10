ffc87aec7f40fae14f103f6e76e629ed98cbbf88
Hugo Benicio <hbobenicio@gmail.com>
2016-10-10 18:09:07 -0300

    Adding more unit tests

--- test/main-test.cpp
+++ test/main-test.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
+#include <string>
+
 #include "fields.h"
+
+using namespace std;
 
 TEST(FieldsTest, CheckIsDecimal)
 {
@@ -10,8 +14,24 @@ TEST(FieldsTest, CheckIsDecimal)
 	ASSERT_TRUE(decimalFieldInfo.isDecimal());
 }
 
-int main(int argc, char* argv[]) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+TEST(FieldsTest, CheckDecimalSeparator)
+{
+	FieldInfo decimalFieldInfo("VA-TRANSACAO", 'N', "16,2");
+
+	ASSERT_EQ(decimalFieldInfo.beforeDecimalSeparator(), "16");
+	ASSERT_EQ(decimalFieldInfo.afterDecimalSeparator(), "2");
 }
 
+TEST(FieldsTest, CheckReadLength)
+{
+	FieldInfo nonDecimalFieldInfo("IT-CO-UNIDADE-GESTORA", 'A', "6");
+	FieldInfo decimalFieldInfo("VA-TRANSACAO", 'N', "16,2");
+
+	ASSERT_EQ(nonDecimalFieldInfo.readLength(), 6);
+	ASSERT_EQ(decimalFieldInfo.readLength(), 18);
+}
+
+int main(int argc, char* argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
